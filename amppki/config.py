2983f686a09db419eeed3c061f6898c06d4abe41
Brendon Jones <brendonj@waikato.ac.nz>
2015-02-09 11:41:08 +1300

    Change the CA directory so it isn't inside amplet.

--- amppki/config.py
+++ amppki/config.py
@@ -1,6 +1,6 @@
 """ Global config options that are used by both the web and cli scripts """
 
-CA_DIR = "/etc/amplet2/pki"
+CA_DIR = "/etc/amppki"
 CERT_DIR = "%s/certs" % CA_DIR
 CSR_DIR = "%s/csr" % CA_DIR
 RABBITMQCTL = "/usr/sbin/rabbitmqctl"
