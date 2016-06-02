75f1d40f313301adf2d3af2bffefacbc7e34dfdd
Brendon Jones <brendonj@waikato.ac.nz>
2016-06-02 17:27:33 +1200

    Bump version number to 0.3 and update changelog.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
     ]
 
 setup(name='amppki',
-      version='0.2',
+      version='0.3',
       description='amp-pki',
       #long_description=README + '\n\n' +  CHANGES,
       long_description="AMP PKI web interface",
