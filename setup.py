15cc53d4109143ebcb368735f2abc88565059e7e
Brendon Jones <brendonj@waikato.ac.nz>
2018-11-09 15:23:29 +1300

    Bump version number to 0.8 and update changelog.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
     ]
 
 setup(name='amppki',
-      version='0.7',
+      version='0.8',
       description='amp-pki',
       #long_description=README + '\n\n' +  CHANGES,
       long_description="AMP PKI web interface",
