fbf3ef436046b74528400ff0352371accaf6aa4f
Brendon Jones <brendonj@waikato.ac.nz>
2019-03-25 16:42:53 +1300

    Update license to GPL version 2.

--- amppki/views/views.py
+++ amppki/views/views.py
@@ -1,3 +1,31 @@
+# This file is part of amppki.
+#
+# Copyright (C) 2015-2019 The University of Waikato, Hamilton, New Zealand.
+#
+# Authors: Brendon Jones
+#
+# All rights reserved.
+#
+# This code has been developed by the WAND Network Research Group at the
+# University of Waikato. For further information please see
+# http://www.wand.net.nz/
+#
+# amppki is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# amppki is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with amppki; if not, write to the Free Software Foundation, Inc.
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Please report any bugs, questions or comments to contact@wand.net.nz
+#
+
 """ Routes used to accept requests and distribute signed certificates """
 
 from base64 import urlsafe_b64decode
